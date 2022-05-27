58ee5cf86f5389748c5853cbac0fede6810d24a2
b3xul <bexul97@gmail.com>
2022-05-27 16:15:22 +0200

    feat: more validation tests for post admin/tickets/

--- TicketCatalogService/src/test/kotlin/it/polito/wa2/g15/lab5/integration/CatalogTests.kt
+++ TicketCatalogService/src/test/kotlin/it/polito/wa2/g15/lab5/integration/CatalogTests.kt
@@ -109,18 +109,53 @@ class CatalogTests {
 
     @Test
     fun invalidAdminTicketsAPI() {
-        val invalidBody = mapOf("type" to "Settimanale", "price" to -1)
+        //val invalidBody = mapOf("type" to "Settimanale","price" to -1)
+        //data class NewTicketItemDTO(
+        //    @field:NotNull
+        //    @field:Positive
+        //    val price: Double,
+        //
+        //    @field:NotBlank(message = "Type can't be empty or null")
+        //    val type: String,
+        //    @field:PositiveOrZero
+        //    val minAge: Int?,
+        //    @field:PositiveOrZero
+        //    val maxAge: Int?,
+        //    @field:Positive
+        //    val duration: Long = -1
+        //)
+        val invalidTickets = listOf(
+            mapOf("price" to null,"type" to "ORDINAL","minAge" to 0, "maxAge" to 200,"duration" to 2000L),
+            mapOf("price" to 0,"type" to "ORDINAL","minAge" to 0, "maxAge" to 200,"duration" to 2000L),
+            mapOf("price" to "x","type" to "ORDINAL","minAge" to 0, "maxAge" to 200,"duration" to 2000L),
+            
+            mapOf("price" to 1,"type" to "","minAge" to 0, "maxAge" to 200,"duration" to 2000L),
+            mapOf("price" to 1,"type" to null,"minAge" to 0, "maxAge" to 200,"duration" to 2000L),
+            
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to -1, "maxAge" to 200,"duration" to 2000L),
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to "x", "maxAge" to 200,"duration" to 2000L),
+            
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to 0, "maxAge" to -1,"duration" to 2000L),
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to 0, "maxAge" to "x","duration" to 2000L),
+            
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to 0, "maxAge" to 200,"duration" to -1),
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to 0, "maxAge" to 200,"duration" to "2000L"),
+            mapOf("price" to 1,"type" to "ORDINAL","minAge" to 0, "maxAge" to 200,"duration" to null)
+        )
+    
 //        {
 //            "type": "Settimanale",
 //            "price": Error
 //        }
-        client.post()
-            .uri("admin/tickets/")
-            .bodyValue(invalidBody)
-            .header(HttpHeaders.CONTENT_TYPE, "application/json")
-            .header(HttpHeaders.AUTHORIZATION,"Bearer " + generateJwtToken("BigBoss", setOf("ADMIN","CUSTOMER")))
-            .exchange()
-            .expectStatus().isBadRequest
+        invalidTickets.forEach {
+            client.post()
+                .uri("admin/tickets/")
+                .bodyValue(it)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + generateJwtToken("BigBoss", setOf("ADMIN", "CUSTOMER")))
+                .exchange()
+                .expectStatus().isBadRequest
+        }
     }
 
     @Test
