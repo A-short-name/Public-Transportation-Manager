294de58ca5bb3bce3b8395288bd1279564c69cf8
MattiaRiola <s280169@studenti.polito.it>
2022-05-27 21:02:27 +0200

    fix: in main init #19

--- TicketCatalogService/src/main/kotlin/it/polito/wa2/g15/lab5/Lab5TicketCatalogService.kt
+++ TicketCatalogService/src/main/kotlin/it/polito/wa2/g15/lab5/Lab5TicketCatalogService.kt
@@ -1,13 +1,23 @@
 package it.polito.wa2.g15.lab5
 
 
+import it.polito.wa2.g15.lab5.services.TicketCatalogService
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Lab5TicketCatalogService
 
+@Autowired
+lateinit var ticketCatalogService: TicketCatalogService
 
 fun main(args: Array<String>) {
     runApplication<Lab5TicketCatalogService>(*args)
+
+    runBlocking {
+        ticketCatalogService.initTicketCatalogCache()
+    }
+
 }
