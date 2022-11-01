ed1ce343713acb4596deaa133e6445edf7ecff54
MattiaRiola <s280169@studenti.polito.it>
2022-11-01 19:12:44 +0100

    🧱added new submodule ValidatorService

---
+++ ValidatorService/src/main/kotlin/it/polito/wa2/g15/validatorservice/ValidatorServiceApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.g15.validatorservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidatorServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ValidatorServiceApplication>(*args)
+}
---
+++ ValidatorService/src/test/kotlin/it/polito/wa2/g15/validatorservice/ValidatorServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.g15.validatorservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValidatorServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
