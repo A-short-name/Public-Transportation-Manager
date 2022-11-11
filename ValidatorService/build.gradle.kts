2cef7b07492d0414b869d2ebb8e3c8e6ca8b69cc
GrayNeel <paoul32@yahoo.it>
2022-11-11 10:32:10 +0100

    feat: jsonwebtoken dependency added

--- ValidatorService/build.gradle.kts
+++ ValidatorService/build.gradle.kts
@@ -20,5 +20,8 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.testcontainers:junit-jupiter:1.16.3")
     testImplementation("org.testcontainers:postgresql:1.16.3")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 tasks.register("prepareKotlinBuildScriptModel"){}
