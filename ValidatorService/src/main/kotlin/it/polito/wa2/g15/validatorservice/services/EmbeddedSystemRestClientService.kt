279319f0cc9449cbfc077b3eff28017e92870b26
GrayNeel <paoul32@yahoo.it>
2023-01-14 20:00:52 +0100

    feat: ValidatorService is now contacting Traveler and Login through Eureka

--- ValidatorService/src/main/kotlin/it/polito/wa2/g15/validatorservice/services/EmbeddedSystemRestClientService.kt
+++ ValidatorService/src/main/kotlin/it/polito/wa2/g15/validatorservice/services/EmbeddedSystemRestClientService.kt
@@ -1,5 +1,6 @@
 package it.polito.wa2.g15.validatorservice.services
 
+import com.netflix.discovery.EurekaClient
 import it.polito.wa2.g15.validatorservice.dtos.UserLoginRequestDTO
 import it.polito.wa2.g15.validatorservice.dtos.UserLoginResponseDTO
 import it.polito.wa2.g15.validatorservice.exceptions.ValidatorLoginException
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.*
 import org.springframework.security.web.csrf.CsrfTokenRepository
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 import org.springframework.web.client.postForEntity
@@ -34,10 +36,8 @@ class EmbeddedSystemRestClientService {
     @Autowired
     lateinit var csrfTokenRepository: CsrfTokenRepository
 
-    //TODO: manage the uri by spring cloud
-    val loginUri: String = "http://localhost:8080/user/login"
-
-    val travelerServiceUri: String = "http://localhost:8081/validation/secret"
+    @Autowired
+    lateinit var discoveryClient : EurekaClient
 
     fun getValidationKey(): String {
         if(loginToken==""){
@@ -49,55 +49,74 @@ class EmbeddedSystemRestClientService {
     }
 
     private fun askValidationKeyToTravelerService(): String {
-        val restTemplate = RestTemplate()
-        val headerBySecConfig = securityConfig.generateCsrfHeader(csrfTokenRepository)
-        headerBySecConfig.contentType = MediaType.APPLICATION_JSON
-        headerBySecConfig.setBearerAuth(loginToken)
-        val request2 = HttpEntity(
-            "",
-            headerBySecConfig
-        )
+        try {
+            val instanceInfo = discoveryClient.getNextServerFromEureka("traveler", false)
+            val baseUri = instanceInfo.homePageUrl
+            val travelerServiceUri = baseUri + "validation/secret"
 
-        val response: ResponseEntity<String> = restTemplate.exchange(
-            travelerServiceUri,
-            HttpMethod.GET,
-            request2
-        )
-        if (response.statusCode != HttpStatus.OK)
-            throw ValidatorLoginException(
-                "can not retrieve validation key from traveler service at: $loginUri" +
-                        "\ntraveler service response: $response"
-            )
-        if (response.body == null)
-            throw ValidatorLoginException(
-                "can not retrieve validation key from traveler service at: $loginUri" +
-                        "\nbecause the key received is null"
+            val restTemplate = RestTemplate()
+            val headerBySecConfig = securityConfig.generateCsrfHeader(csrfTokenRepository)
+            headerBySecConfig.contentType = MediaType.APPLICATION_JSON
+            headerBySecConfig.setBearerAuth(loginToken)
+            val request2 = HttpEntity(
+                "",
+                headerBySecConfig
             )
 
-        return response.body!!
+            val response: ResponseEntity<String> = restTemplate.exchange(
+                travelerServiceUri,
+                HttpMethod.GET,
+                request2
+            )
+            if (response.statusCode != HttpStatus.OK)
+                throw ValidatorLoginException(
+                    "can not retrieve validation key from traveler service" +
+                            "\ntraveler service response: $response"
+                )
+            if (response.body == null)
+                throw ValidatorLoginException(
+                    "can not retrieve validation key from traveler service" +
+                            "\nbecause the key received is null"
+                )
+            return response.body!!
+        }catch(e: RuntimeException) {
+            throw ValidatorLoginException("Traveler service not found")
+        }catch(e: RestClientException) {
+            throw ValidatorLoginException("Problems contacting traveler service")
+        }
     }
 
 
     fun performLogin() {
+        try {
+            val instanceInfo = discoveryClient.getNextServerFromEureka("login", false)
+            val baseUri = instanceInfo.homePageUrl
+            val loginUri = baseUri + "user/login"
 
-        val restTemplate = RestTemplate()
-        val headerBySecConfig = securityConfig.generateCsrfHeader(csrfTokenRepository)
-        headerBySecConfig.contentType = MediaType.APPLICATION_JSON
+            val restTemplate = RestTemplate()
+            val headerBySecConfig = securityConfig.generateCsrfHeader(csrfTokenRepository)
+            headerBySecConfig.contentType = MediaType.APPLICATION_JSON
 
-        val request2 = HttpEntity(
-            UserLoginRequestDTO(nickname = validatorUserName, password = validatorPassword),
-            headerBySecConfig
-        )
+            val request2 = HttpEntity(
+                UserLoginRequestDTO(nickname = validatorUserName, password = validatorPassword),
+                headerBySecConfig
+            )
 
-        val response: ResponseEntity<UserLoginResponseDTO> = restTemplate.postForEntity(
-            loginUri, request2
-        )
+            val response: ResponseEntity<UserLoginResponseDTO> = restTemplate.postForEntity(
+                loginUri, request2
+            )
 
-        when (response.statusCode) {
-            HttpStatus.OK -> loginToken = response.body!!.token
-            HttpStatus.NOT_FOUND -> throw ValidatorLoginException("login server not found in uri: $loginUri")
-            HttpStatus.TOO_MANY_REQUESTS -> throw ValidatorLoginException("login server too many login requests in uri: $loginUri")
-            else -> throw ValidatorLoginException("response not ok to uri: $loginUri\n got response: $response")
+            when (response.statusCode) {
+                HttpStatus.OK -> loginToken = response.body!!.token
+                HttpStatus.NOT_FOUND -> throw ValidatorLoginException("login server not found in uri: $loginUri")
+                HttpStatus.TOO_MANY_REQUESTS -> throw ValidatorLoginException("login server too many login requests in uri: $loginUri")
+                else -> throw ValidatorLoginException("response not ok to uri: $loginUri\n got response: $response")
+            }
+
+        }catch(e: RuntimeException) {
+            throw ValidatorLoginException("Login service not found")
+        }catch(e: RestClientException) {
+            throw ValidatorLoginException("Problems contacting login service")
         }
     }
 }
