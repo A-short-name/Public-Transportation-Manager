683c47d0d2e267db49c9d776e1fc69f9c10ec03b
GrayNeel <paoul32@yahoo.it>
2023-01-14 16:43:18 +0100

    feat: working eureka server on LoginService

--- EurekaServerService/src/main/kotlin/it/polito/wa2/g15/eurekaserverservice/EurekaServerServiceApplication.kt
+++ EurekaServerService/src/main/kotlin/it/polito/wa2/g15/eurekaserverservice/EurekaServerServiceApplication.kt
@@ -2,8 +2,10 @@ package it.polito.wa2.g15.eurekaserverservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaServerServiceApplication
 
 fun main(args: Array<String>) {
