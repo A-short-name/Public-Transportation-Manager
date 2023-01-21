19daf2ffbb952dba99b4a36fda025e67bc7f13c2
lelexdrugo <gabrielescaffidi@gmail.com>
2023-01-21 17:25:04 +0100

    feat: encrypted password for superadmin

--- LoginService/src/main/resources/data.sql
+++ LoginService/src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO roles(id,name) VALUES(3,'CUSTOMER'); /* avoid throwing exception is 
 INSERT INTO roles(id,name) VALUES(4,'EMBEDDED'); /* avoid throwing exception is role already present */
 INSERT INTO roles(id,name) VALUES(5,'SERVICE'); /* avoid throwing exception is role already present */
 /*bcrypted password superadmin = $2a$12$5UL/sjysEtyD3m3i7/xHmupbWCfdgAHq5EYPIWOZnFvk4X8NWg/AW */
-INSERT INTO users(id,active,email,nickname,password) VALUES(1,'true','superadmin@me.com','superadmin','superadmin');
+INSERT INTO users(id,active,email,nickname,password) VALUES(1,'true','superadmin@me.com','superadmin','$2a$12$5UL/sjysEtyD3m3i7/xHmupbWCfdgAHq5EYPIWOZnFvk4X8NWg/AW');
 /*bctripted password validator1 = $2a$12$s6oYucvWXPfeUmSjGMmnDudw.d1YunSucW90B7Bn5ABEOIa4EvwtW */
 INSERT INTO users(id,active,email,nickname,password) VALUES(2,'true','val1@me.com','validator1','$2a$12$s6oYucvWXPfeUmSjGMmnDudw.d1YunSucW90B7Bn5ABEOIa4EvwtW');
 INSERT INTO user_roles(user_id,role_id) VALUES(1,2);
