d7e240d05be1855c6601366f169d06ca428363ae
GrayNeel <paoul32@yahoo.it>
2023-01-25 11:39:34 +0100

    fix: bug in superadmin's password hash

--- LoginService/src/main/resources/data.sql
+++ LoginService/src/main/resources/data.sql
@@ -5,8 +5,8 @@ INSERT INTO roles(id,name) VALUES(2,'SUPERADMIN'); /* avoid throwing exception i
 INSERT INTO roles(id,name) VALUES(3,'CUSTOMER'); /* avoid throwing exception is role already present */
 INSERT INTO roles(id,name) VALUES(4,'EMBEDDED'); /* avoid throwing exception is role already present */
 INSERT INTO roles(id,name) VALUES(5,'SERVICE'); /* avoid throwing exception is role already present */
-/*bcrypted password superadmin = $2a$12$5UL/sjysEtyD3m3i7/xHmupbWCfdgAHq5EYPIWOZnFvk4X8NWg/AW */
-INSERT INTO users(id,active,email,nickname,password) VALUES(1,'true','superadmin@me.com','superadmin','$2a$12$5UL/sjysEtyD3m3i7/xHmupbWCfdgAHq5EYPIWOZnFvk4X8NWg/AW');
+/*bcrypted password superadmin = $2a$12$QtjvT9TWdbPcTADJpO4E1uWTO3IMzuY9EoZ5RTSSmaNGzbqI0P7pW */
+INSERT INTO users(id,active,email,nickname,password) VALUES(1,'true','superadmin@me.com','superadmin','$2a$12$QtjvT9TWdbPcTADJpO4E1uWTO3IMzuY9EoZ5RTSSmaNGzbqI0P7pW');
 /*bctripted password validator1 = $2a$12$s6oYucvWXPfeUmSjGMmnDudw.d1YunSucW90B7Bn5ABEOIa4EvwtW */
 INSERT INTO users(id,active,email,nickname,password) VALUES(2,'true','val1@me.com','validator1','$2a$12$s6oYucvWXPfeUmSjGMmnDudw.d1YunSucW90B7Bn5ABEOIa4EvwtW');
 INSERT INTO user_roles(user_id,role_id) VALUES(1,2);
